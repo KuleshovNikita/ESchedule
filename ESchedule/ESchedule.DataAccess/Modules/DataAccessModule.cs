e1c419823051327997cf960778448e1d936f2bc2
Mykyta <mykyta.kuleshov@nure.ua>
2023-04-02 20:38:52 +0300

    added repositories for many-to-many models

--- ESchedule/ESchedule.DataAccess/Modules/DataAccessModule.cs
+++ ESchedule/ESchedule.DataAccess/Modules/DataAccessModule.cs
@@ -1,9 +1,12 @@
 ﻿using ESchedule.DataAccess.Repos;
 using ESchedule.DataAccess.Repos.Groups;
 using ESchedule.DataAccess.Repos.Lessons;
+using ESchedule.DataAccess.Repos.ManyToMany;
 using ESchedule.DataAccess.Repos.Tenant;
 using ESchedule.DataAccess.Repos.User;
 using ESchedule.Domain.Lessons;
+using ESchedule.Domain.Lessons.Schedule;
+using ESchedule.Domain.ManyToManyModels;
 using ESchedule.Domain.Modules;
 using ESchedule.Domain.Tenant;
 using ESchedule.Domain.Users;
@@ -23,6 +26,9 @@ namespace ESchedule.DataAccess.Modules
             services.AddScoped<IRepository<TenantSettingsModel>, TenantSettingsRepository>();
             services.AddScoped<IRepository<ScheduleModel>, ScheduleRepository>();
             services.AddScoped<IRepository<TenantModel>, TenantRepository>();
+            services.AddScoped<IRepository<TeachersLessonsModel>, TeachersLessonsRepository>();
+            services.AddScoped<IRepository<TeachersGroupsLessonsModel>, TeachersGroupsLessonsRepository>();
+            services.AddScoped<IRepository<GroupsLessonsModel>, GroupsLessonsRepository>();
 
             return services;
         }
---
+++ ESchedule/ESchedule.DataAccess/Repos/ManyToMany/TeachersGroupsLessonsRepository.cs
@@ -0,0 +1,57 @@
+﻿using ESchedule.DataAccess.Context;
+using ESchedule.Domain.Exceptions;
+using ESchedule.Domain.ManyToManyModels;
+using ESchedule.ServiceResulting;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace ESchedule.DataAccess.Repos.ManyToMany
+{
+    public class TeachersGroupsLessonsRepository : Repository<TeachersGroupsLessonsModel>
+    {
+        public TeachersGroupsLessonsRepository(EScheduleDbContext context) : base(context)
+        {
+        }
+
+        public override async Task<ServiceResult<TeachersGroupsLessonsModel>> First(Expression<Func<TeachersGroupsLessonsModel, bool>> command)
+        {
+            var result = new ServiceResult<TeachersGroupsLessonsModel>();
+
+            try
+            {
+                result.Value = await _context.Set<TeachersGroupsLessonsModel>()
+                    .Include(x => x.Teacher)
+                    .Include(x => x.Lesson)
+                    .Include(x => x.StudyGroup)
+                    .FirstOrDefaultAsync(command) ?? throw new EntityNotFoundException();
+
+                return result.Success();
+            }
+            catch (Exception ex)
+            {
+                return result.Fail(ex);
+            }
+        }
+
+        public override Task<ServiceResult<IEnumerable<TeachersGroupsLessonsModel>>> Where(Expression<Func<TeachersGroupsLessonsModel, bool>> command)
+        {
+            var result = new ServiceResult<IEnumerable<TeachersGroupsLessonsModel>>();
+
+            try
+            {
+                result.Value = _context.Set<TeachersGroupsLessonsModel>()
+                    .Include(x => x.Teacher)
+                    .Include(x => x.Lesson)
+                    .Include(x => x.StudyGroup)
+                    .Where(command)
+                        ?? throw new EntityNotFoundException();
+
+                return Task.FromResult(result.Success());
+            }
+            catch (Exception ex)
+            {
+                return Task.FromResult(result.Fail(ex));
+            }
+        }
+    }
+}
