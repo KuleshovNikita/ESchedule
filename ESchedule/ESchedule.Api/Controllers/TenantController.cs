0b23793184ce3fff2e6260c33911c06473074603
Mykyta <garrysm8@gmail.com>
2024-05-01 13:30:21 +0300

    code clean up

--- ESchedule/ESchedule.Api/Controllers/TenantController.cs
+++ ESchedule/ESchedule.Api/Controllers/TenantController.cs
@@ -2,11 +2,7 @@
 using ESchedule.Api.Models.Updates;
 using ESchedule.Business;
 using ESchedule.Business.Tenant;
-using ESchedule.Business.Users;
-using ESchedule.Domain.Lessons;
-using ESchedule.Domain.Policy;
 using ESchedule.Domain.Tenant;
-using ESchedule.Domain.Users;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,18 +10,10 @@ namespace ESchedule.Api.Controllers
 {
     public class TenantController : BaseController<TenantModel>
     {
-        private readonly IUserService _userService;
         private readonly ITenantService _tenantService;
-        private readonly IBaseService<GroupModel> _groupService;
-        private readonly IBaseService<LessonModel> _lessonService;
 
-        public TenantController(IBaseService<TenantModel> service, IUserService userService,
-            IBaseService<GroupModel> groupService, IBaseService<LessonModel> lessonService,
-            ITenantService tenantService) : base(service)
+        public TenantController(IBaseService<TenantModel> service, ITenantService tenantService) : base(service)
         {
-            _userService = userService;
-            _groupService = groupService;       
-            _lessonService = lessonService;
             _tenantService = tenantService;
         }
 
