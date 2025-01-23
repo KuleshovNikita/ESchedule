f384e967bfbb0af37600a34e16ffb2f6dfa3fdfe
Mykyta <garrysm8@gmail.com>
2025-01-23 23:11:26 +0200

    @es-64 migrate the Business project to .net 8

--- ESchedule/ESchedule.Business/Email/EmailService.cs
+++ ESchedule/ESchedule.Business/Email/EmailService.cs
@@ -5,27 +5,22 @@ using Microsoft.Extensions.Configuration;
 
 namespace ESchedule.Business.Email
 {
-    public class EmailService : IEmailService
+    public class EmailService(
+        IConfiguration config, 
+        IEmailMessageClient messageClient
+    ) 
+        : IEmailService
     {
-        private readonly IConfiguration _config;
-        private readonly IEmailMessageClient _messageClient;
-
-        public EmailService(IConfiguration config, IEmailMessageClient messageClient)
-        {
-            _config = config;
-            _messageClient = messageClient;
-        }
-
         public async Task SendConfirmEmailMessage(UserModel userModel)
         {
             var confirmUrl = BuildConfirmUrl(userModel);
             var message = BuildEmailMessage(confirmUrl);
-            await _messageClient.SendEmail(message, userModel.Login);
+            await messageClient.SendEmail(message, userModel.Login);
         }
 
         private string BuildConfirmUrl(UserModel userModel)
         {
-            var serverUrl = _config.GetSection("ClientUrl").Value;
+            var serverUrl = config.GetSection("ClientUrl").Value;
 
             var encodedHashKey = Uri.EscapeDataString(userModel.Password);
             var confirmEndpoint = $"/confirmEmail/{encodedHashKey}";
