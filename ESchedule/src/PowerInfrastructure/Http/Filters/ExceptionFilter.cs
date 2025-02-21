a3248c34453b7a6bc19036c41b47c5604a48ec68
Mykyta <garrysm8@gmail.com>
2025-02-21 21:53:06 +0200

    es-68 add logging to exception filter

--- ESchedule/src/PowerInfrastructure/Http/Filters/ExceptionFilter.cs
+++ ESchedule/src/PowerInfrastructure/Http/Filters/ExceptionFilter.cs
@@ -1,16 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using PowerInfrastructure.Exceptions;
 using PowerInfrastructure.Extensions;
 using System.Security.Authentication;
 
 namespace PowerInfrastructure.Http.Filters;
 
-public class ExceptionFilter(IHostEnvironment environment) : IExceptionFilter
+public class ExceptionFilter(IHostEnvironment environment, ILogger<ExceptionFilter> logger) : IExceptionFilter
 {
     public void OnException(ExceptionContext context)
     {
+        logger.LogError(context.Exception, "Unhandled error occured");
+
         var statusCode = context.Exception switch
         {
             ItemNotFoundException => 404,
